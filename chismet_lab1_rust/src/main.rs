a404b61578f0e3cde98de1e507afb7dbde7c21b1
taras <luviq7@gmail.com>
2025-03-15 03:09:24 +0200

    fix

--- chismet_lab1_rust/src/main.rs
+++ chismet_lab1_rust/src/main.rs
@@ -2,9 +2,10 @@ mod functions;
 fn main() {
     let a: f64 = 6.0;
     let b: f64 = 7.0;
-    let eps: f64 = 0.001;
+    let eps: f64 = 0.00001;
     println!("Метод дихотомії");
     functions::dix(0, a, b, eps);
     println!("Метод хорд");
     functions::hord(0, a, b, eps);
+
 }
